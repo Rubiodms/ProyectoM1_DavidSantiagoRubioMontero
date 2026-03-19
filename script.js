13ede55a2544ffcdc0685ff81d30aae3ebe46d5c
RubioDMS <dvdsntgrubio@gmail.com>
2026-03-19 11:31:57 -0500

    Feat:Se agrega logica para agregar y quitar paletas de colores

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const seleccionador = document.getElementById('boton-seleccionador');
+const paletas = document.getElementsByClassName('caja-color');
+const contenedor_cajas = document.getElementById('contenedor-cajas'); //selecionamos el contenedor de las cajas del DOM
 
 seleccionador.addEventListener('change', function() {
-    // 'this.value' toma el valor del atributo 'value' del option seleccionado
+    let hijos_actuales = contenedor_cajas.childElementCount;
+
     if (this.value === "6-colores") {
-        console.log("Elegiste 6 colores");
+        // Si hay más de 6, quitamos las que sobran
+        for (let i = hijos_actuales; i > 6; i--) {
+            contenedor_cajas.removeChild(contenedor_cajas.lastElementChild);
+        }
+
     } else if (this.value === "8-colores") {
-        console.log("Elegiste 8 colores");
+        // Lógica para llegar a 8
+        for (let i = hijos_actuales; i < 8; i++) {
+            let nueva_caja = document.createElement('div'); // CREADA ADENTRO
+            nueva_caja.innerHTML = '<p>HSL:<br>RGBA:</p>';
+            nueva_caja.classList.add("caja-color");
+            contenedor_cajas.appendChild(nueva_caja);
+        }
+
     } else if (this.value === "9-colores") {
-        console.log("Elegiste 9 colores");
+        // Lógica para llegar a 9
+        for (let i = hijos_actuales; i < 9; i++) {
+            let nueva_caja = document.createElement('div'); // CREADA ADENTRO
+            nueva_caja.innerHTML = '<p>HSL:<br>RGBA:</p>';
+            nueva_caja.classList.add("caja-color");
+            contenedor_cajas.appendChild(nueva_caja);
+        }
     }
 });
-
